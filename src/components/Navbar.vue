20ad1530315b4c0873d91e65b4933b0208f6256d
何青山 <10931158+h-qingshan@user.noreply.gitee.com>
2023-04-20 22:32:07 +0800

    导航栏

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar navbar-light
+    bg-white
+    navbar-expand-lg
+    border-bottom
+    fixed-top">
+      <div class="container">
+          <a href="/" class="navbar-brand"><strong>Vue</strong>图片上传</a>
+          <form action="" class="form-inline mt-2 mt-md-0">
+              <input type="text" class="form-control" placeholder="Search in Drive" aria-label="Search">
+          </form>
+      </div>
+    </nav>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
